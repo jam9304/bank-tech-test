db76758b2a9fbd9ad5a23e09f1c8412411d7fd1d
James Mooney <jmooney9304@gmail.com>
2022-04-26 13:17:47 +0100

    statement class can now store a list of transaction in an array

--- statement.js
+++ statement.js
@@ -1,11 +1,16 @@
 class Statement {
   constructor () {
     this.header = "Date || Credit || Debit || Balance\n";
+    this.transactions = [];
+  };
+
+  addTransactions(transaction) {
+    this.transactions.push(transaction)
   };
 
   print() {
     return this.header
-  }
+  };
 };
 
 module.exports = Statement;
--- statement.test.js
+++ statement.test.js
@@ -1,10 +1,30 @@
 // const { describe } = require('eslint/lib/rule-tester/rule-tester');
 // const { it } = require('eslint/lib/rule-tester/rule-tester');
+// const { it } = require('eslint/lib/rule-tester/rule-tester');
 const Statement = require('./statement');
 
 describe('Statement', () => {
   it('prints the statement header', () => {
     const statement = new Statement()
     expect(statement.print()).toBe("Date || Credit || Debit || Balance\n")
+  });
+
+  it('adds transaction to transaction array', () => {
+    const statement = new Statement()
+    statement.addTransactions({
+      Date: "01/01/22",
+      Withdraw: "",
+      Deposit: "10",
+      Balance: "10"
+    })
+    expect(statement.transactions).toEqual([{"Balance": "10", "Date": "01/01/22", "Deposit": "10", "Withdraw": ""}]);
+    statement.addTransactions({
+      Date: "02/01/22",
+      Withdraw: "",
+      Deposit: "10",
+      Balance: "20"
+    })
+    expect(statement.transactions).toEqual([{"Balance": "10", "Date": "01/01/22", "Deposit": "10", "Withdraw": ""}, {"Balance": "20", "Date": "02/01/22", "Deposit": "10", "Withdraw": ""}]);
+
   });
 });
